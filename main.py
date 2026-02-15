58a44d5e32bdbb7b6684187fce9faae876ae272a
Mohammad Al-Ahdal <mo@mohammad.dev>
2026-02-15 06:01:23 -0700

    Enable CORS for all origins and methods

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from model import converter
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
 
 
 class ImageRequest(BaseModel):
