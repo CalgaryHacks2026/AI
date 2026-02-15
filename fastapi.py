dc45051facd52f8b94f68ab15a2825219449c451
Mohammad Al-Ahdal <mo@mohammad.dev>
2026-02-15 05:26:35 -0700

    Add root hello world endpoint

--- fastapi.py
+++ fastapi.py
@@ -1,14 +1,22 @@
-from fastapi import FastAPI
 from pydantic import BaseModel
+
+from fastapi import FastAPI
 from model import converter
 
 app = FastAPI()
+
 
 class ImageRequest(BaseModel):
     image_url: str
     tags: list[str]
 
+
 @app.post("/image-to-tags")
 def convert_image(request: ImageRequest):
     result = converter(request.tags, request.image_url)
     return result
+
+
+@app.get("/")
+def hello():
+    return {"message": "Hello World"}
